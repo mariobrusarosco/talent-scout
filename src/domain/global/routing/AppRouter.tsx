b9c702729778a7676053c935390f119fae1af265
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-01-18 20:16:16 -0300

    Routing: Basic Nesting

--- src/domain/global/routing/AppRouter.tsx
+++ src/domain/global/routing/AppRouter.tsx
@@ -3,16 +3,31 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
 import DashboardScreen from "../../dashboard/screen/Dashboard";
 import AppLayout from "../components/AppLayout";
 import HomeScreen from "../screens/Home";
 
+const TournamentLayout = () => (
+  <section>
+    <header>Tournaments</header>
+    <Outlet />
+  </section>
+);
+
+const ActiveScreen = () => <div>active</div>;
+const UnactiveScreen = () => <div>unactive</div>;
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<HomeScreen />} />
-      <Route path="/dashboard" element={<DashboardScreen />} />
+      <Route path="dashboard" element={<DashboardScreen />} />
+      <Route path="tournaments" element={<TournamentLayout />}>
+        <Route path="active" element={<ActiveScreen />} />
+        <Route path="inactive" element={<UnactiveScreen />} />
+      </Route>
     </Route>
   )
 );
