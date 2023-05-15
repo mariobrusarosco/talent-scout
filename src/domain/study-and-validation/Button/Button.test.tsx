71124a800cfca23a62ef0eda8d74d3c9785dd961
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-05-15 09:48:43 -0300

    function to render button - non-learning stuff

--- src/domain/study-and-validation/Button/Button.test.tsx
+++ src/domain/study-and-validation/Button/Button.test.tsx
@@ -2,22 +2,24 @@ import { Button } from "./Button";
 import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
+const renderComponent = (props?: any) => {
+  return render(<Button onClick={jest.fn()} {...props} />);
+};
+
 describe("Button", () => {
   describe("when rendering", () => {
     it("renders the element", () => {
-      render(<Button />);
+      renderComponent();
 
       expect(
-        screen.getByRole("button", { name: "Hello World: 0" })
+        screen.getByRole("button", { name: "Hello World" })
       ).toBeInTheDocument();
     });
 
     it("renders the expected styles", () => {
-      render(<Button />);
+      renderComponent();
 
-      expect(
-        screen.getByRole("button", { name: "Hello World: 0" })
-      ).toHaveStyle({
+      expect(screen.getByRole("button", { name: "Hello World" })).toHaveStyle({
         "background-color": "red",
       });
       // Notes
@@ -40,19 +42,20 @@ describe("Button", () => {
     });
 
     it("renders the expected text context", () => {
-      render(<Button />);
+      renderComponent();
 
-      const button = screen.getByRole("button", { name: "Hello World: 0" });
-      expect(button).toHaveTextContent("Hello World: 0");
+      const button = screen.getByRole("button", { name: "Hello World" });
+      expect(button).toHaveTextContent("Hello World");
     });
 
-    it.only("increments when clicked", () => {
-      render(<Button />);
-      const button = screen.getByRole("button", { name: "Hello World: 0" });
+    it("gets disabled", () => {
+      // TODO use rerender here!
+      renderComponent();
+      const button = screen.getByRole("button", { name: "Hello World" });
 
       fireEvent.click(button);
 
-      expect(button).toHaveTextContent("Hello World: 1");
+      expect(button).toHaveTextContent("Hello World");
     });
   });
 });
